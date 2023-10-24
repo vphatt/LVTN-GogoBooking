aaf6b2c544a3709c39fbd2ec94393457f8335bc4
phatB1910276 <phatB1910276@student.ctu.edu.vn>
2023-10-24 14:44:21 +0700

    Kiem tra dang nhap

--- mobile/driver_app/lib/controller/splash_controller.dart
+++ mobile/driver_app/lib/controller/splash_controller.dart
@@ -2,6 +2,8 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:driver_app/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -90,7 +92,10 @@ class SplashController extends GetxController {
     //     :
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
+      MaterialPageRoute(
+          builder: (context) => FirebaseAuth.instance.currentUser == null
+              ? const LoginScreen()
+              : const HomePage()),
     );
   }
 }
